d3c28dce77a20d9ccbb23e094938f14175e9ad5d
Gordon Koo <gordon@opendoor.com>
2021-12-08 22:01:23 -0800

    preliminary w/ grid

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-} from "react-router-dom";
+} from 'react-router-dom';
 
 import Homepage from './components/Homepage';
 import Builder from './components/Builder';
--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,25 +1,59 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
+import { styled } from '@mui/material/styles';
 import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
 
 import Layout from './Layout';
-import Puzzle from '../utils/puzzle';
-import PuzzleView from './PuzzleView';
+import vaccineImg from '../images/vaccine.jpg';
+import './Homepage.css';
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+// A custom hook that builds on useLocation to parse
+// the query string for you.
+function useQuery() {
+  const { search } = useLocation();
+
+  return React.useMemo(() => new URLSearchParams(search), [search]);
+}
 
 const Homepage = () => {
-  const puzzle = new Puzzle(5, 5, [
-    [1, 0, 0, 0, 1],
-    [0, 0, 0, 0, 0],
-    [0, 1, 0, 1, 0],
-    [1, 0, 0, 0, 1],
-    [1, 1, 0, 1, 1],
-  ]);
+  const query = useQuery();
+  const page = query.get('page');
+
+  if (!page) {
+    return (
+      <Container>
+        <Grid container spacing={2}>
+          <Grid item xs={6} md={8}>
+            <Item>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</Item>
+          </Grid>
+          <Grid item xs={6} md={4}>
+            <Item id='photo-1' className='photo'/>
+          </Grid>
+          <Grid item xs={6} md={4}>
+            <Item id='photo-2' className='photo'/>
+          </Grid>
+          <Grid item xs={6} md={8}>
+            <Item>Here is another thing</Item>
+          </Grid>
+        </Grid>
+      </Container>
+    );
+  }
 
   return (
     <Layout>
       <Container>
-        <h1>Hello, world!</h1>
-        <PuzzleView puzzle={puzzle}/>
+        <h1>Location is: {query.get('page')}</h1>
       </Container>
     </Layout>
   );
