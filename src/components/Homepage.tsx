db4b36e9e0354a0fed23bb3e28114f93f7033001
Gordon Koo <gordon@opendoor.com>
2021-12-11 13:14:57 -0800

    Layout and stuff

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -38,46 +38,48 @@ function Item(props: any) {
 
 const Homepage = () => {
   const query = useQuery();
-  const page = query.get('page');
+  const code = query.get('code');
 
-  if (!page) {
-    return (
-      <Container>
-        <Box className='holiday-card-row' sx={{
-          p: 1,
-          bgcolor: 'background.paper',
-        }}>
-          <Item id='photo-1' sx={{ flexGrow: 1 }} className='photo'/>
-          <Item id='photo-2' sx={{ flexGrow: 1 }} className='photo'/>
-          <Item id='photo-3' sx={{ flexGrow: 1 }} className='photo'/>
-          <Item id='photo-4' sx={{ flexGrow: 1 }} className='photo'/>
-        </Box>
-        <Box className='holiday-card-row' sx={{
-          p: 1,
-          bgcolor: 'background.paper',
-        }}>
-          <Item sx={{ flexShrink: 2 }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</Item>
-          <Item id='photo-5' className='photo'/>
-        </Box>
-        <Box className='holiday-card-row' sx={{
-          p: 1,
-          bgcolor: 'background.paper',
-        }}>
-          <Item id='photo-6' sx={{ flexGrow: 1 }} className='photo'/>
-          <Item id='photo-7' sx={{ flexGrow: 1 }} className='photo'/>
-          <Item id='photo-8' sx={{ flexGrow: 1 }} className='photo'/>
-          <Item id='photo-9' sx={{ flexGrow: 1 }} className='photo'/>
-        </Box>
-      </Container>
-    );
+  let msg;
+
+  switch (code) {
+    case 'gordon':
+      msg = 'You suck!';
+      break;
+    default:
+      msg = 'Wishing a happy holiday season and amazing new year to you and your family!';
   }
 
   return (
-    <Layout>
-      <Container>
-        <h1>Location is: {query.get('page')}</h1>
-      </Container>
-    </Layout>
+    <Container sx={{ mt: 6 }}>
+      <Box className='holiday-card-row' sx={{
+        bgcolor: 'background.paper',
+      }}>
+        <Item id='photo-1' sx={{ flexGrow: 1 }} className='photo'/>
+        <Item id='photo-2' sx={{ flexGrow: 1 }} className='photo'/>
+        <Item id='photo-3' sx={{ flexGrow: 1 }} className='photo'/>
+        <Item id='photo-4' sx={{ flexGrow: 1 }} className='photo'/>
+      </Box>
+      <Box className='holiday-card-row' sx={{
+        bgcolor: 'background.paper',
+      }}>
+        <Item sx={{ flexGrow: 2 }} id='greeting'>
+          <h2 className='greeting-header'>HAPPY HOLIDAYS</h2>
+          <p className='handwriting'>
+            {msg}
+          </p>
+        </Item>
+        <Item id='photo-5' sx={{ flexGrow: 1 }} className='photo'/>
+      </Box>
+      <Box className='holiday-card-row' sx={{
+        bgcolor: 'background.paper',
+      }}>
+        <Item id='photo-6' sx={{ flexGrow: 1 }} className='photo'/>
+        <Item id='photo-7' sx={{ flexGrow: 1 }} className='photo'/>
+        <Item id='photo-8' sx={{ flexGrow: 1 }} className='photo'/>
+        <Item id='photo-9' sx={{ flexGrow: 1 }} className='photo'/>
+      </Box>
+    </Container>
   );
 };
 
