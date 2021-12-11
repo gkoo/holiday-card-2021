82e1332885b0ff3c3fb5f177ece3d55ad024f0bf
Gordon Koo <gordon@opendoor.com>
2021-12-11 12:38:04 -0800

    basic layout\?

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,21 +1,12 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 
-import { styled } from '@mui/material/styles';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
+import Container from '@mui/material/Container';
 
 import Layout from './Layout';
-import vaccineImg from '../images/vaccine.jpg';
 import './Homepage.css';
-
-const Item = styled(Paper)(({ theme }) => ({
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
 
 // A custom hook that builds on useLocation to parse
 // the query string for you.
@@ -25,6 +16,26 @@ function useQuery() {
   return React.useMemo(() => new URLSearchParams(search), [search]);
 }
 
+function Item(props: any) {
+  const { sx, ...other } = props;
+  return (
+    <Paper
+      sx={{
+        bgcolor: 'background.paper',
+        color: 'black',
+        p: 0.25,
+        m: 0.25,
+        borderRadius: 0,
+        textAlign: 'center',
+        fontSize: '1rem',
+        fontWeight: '700',
+        ...sx,
+      }}
+      {...other}
+    />
+  );
+}
+
 const Homepage = () => {
   const query = useQuery();
   const page = query.get('page');
@@ -32,20 +43,31 @@ const Homepage = () => {
   if (!page) {
     return (
       <Container>
-        <Grid container spacing={2}>
-          <Grid item xs={6} md={8}>
-            <Item>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</Item>
-          </Grid>
-          <Grid item xs={6} md={4}>
-            <Item id='photo-1' className='photo'/>
-          </Grid>
-          <Grid item xs={6} md={4}>
-            <Item id='photo-2' className='photo'/>
-          </Grid>
-          <Grid item xs={6} md={8}>
-            <Item>Here is another thing</Item>
-          </Grid>
-        </Grid>
+        <Box className='holiday-card-row' sx={{
+          p: 1,
+          bgcolor: 'background.paper',
+        }}>
+          <Item id='photo-1' sx={{ flexGrow: 1 }} className='photo'/>
+          <Item id='photo-2' sx={{ flexGrow: 1 }} className='photo'/>
+          <Item id='photo-3' sx={{ flexGrow: 1 }} className='photo'/>
+          <Item id='photo-4' sx={{ flexGrow: 1 }} className='photo'/>
+        </Box>
+        <Box className='holiday-card-row' sx={{
+          p: 1,
+          bgcolor: 'background.paper',
+        }}>
+          <Item sx={{ flexShrink: 2 }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</Item>
+          <Item id='photo-5' className='photo'/>
+        </Box>
+        <Box className='holiday-card-row' sx={{
+          p: 1,
+          bgcolor: 'background.paper',
+        }}>
+          <Item id='photo-6' sx={{ flexGrow: 1 }} className='photo'/>
+          <Item id='photo-7' sx={{ flexGrow: 1 }} className='photo'/>
+          <Item id='photo-8' sx={{ flexGrow: 1 }} className='photo'/>
+          <Item id='photo-9' sx={{ flexGrow: 1 }} className='photo'/>
+        </Box>
       </Container>
     );
   }
